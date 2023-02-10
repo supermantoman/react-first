dce6083c2a9dd80e038e830e40b1cf609049f743
supermantoman <jwg7632@gmail.com>
2023-02-10 14:55:52 +0900

    useState중괄호 문법 연습

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
 
   let post = "강남 우동 맛집";
+  let [a, b] = useState('남자 코트 추천');
 
   return (
     <div className="App">
@@ -15,7 +17,7 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{a}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
