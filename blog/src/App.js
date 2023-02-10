95b2c0dc08ad2b67705cbcc3fe11e6d7628ee0ba
supermantoman <jwg7632@gmail.com>
2023-02-10 15:10:55 +0900

    useState데이터바인딩연습

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 function App() {
 
   let post = "강남 우동 맛집";
-  let [a, b] = useState('남자 코트 추천');
+  let [title, b] = useState(['제목이다 호호호', '케케케', '부부부부']);
 
   return (
     <div className="App">
@@ -13,15 +13,15 @@ function App() {
         <div>개발 blog</div>
       </div>
       <div className="list">
-        <h4>글제목</h4>
+        <h4>{title[0]}</h4>
+        <p>{title}</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{a}</h4>
-        <p>2월 17일 발행</p>
-      </div>
-      <div className="list">
-        <h4>글제목</h4>
+        <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
