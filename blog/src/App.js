04dce76b82d9eb21cb58a0fe22120f236d8de30d
supermantoman <jwg7632@gmail.com>
2023-02-12 02:33:41 +0900

    컴포넌트 만들기 연습

--- blog/src/App.js
+++ blog/src/App.js
@@ -32,8 +32,19 @@ function App() {
         <p>상세내용</p>
       </div>
     </div>
+   
 
   );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+     <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
