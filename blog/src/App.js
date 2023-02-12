47c0cac4c9197bfa8845196a36e1e927db262e1f
supermantoman <jwg7632@gmail.com>
2023-02-12 15:38:00 +0900

    모달창 on/off기능 구현

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,7 +7,7 @@ function App() {
   let post = "강남 우동 맛집";
   let [title, b] = useState(['제목이다 호호호', '케케케', '부부부부']);
   let [따봉, 따봉바꾸기] = useState(0);
-  // let [modal, setModal] = useSate(false);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -23,12 +23,30 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{title[2]}</h4>
+        <h4 onClick={()=>{setModal(!modal)}}>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
-    </div> 
+
+      {/* 모달창생성 */}
+
+    {
+      modal == true ? <Modal/> : null
+    }
+
+    </div>
   );
 }
 
+// 컴포넌트 묶기
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>모달제목입니다</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+  
+}
 
 export default App;
