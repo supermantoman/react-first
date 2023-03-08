2de630d66f2cc58f951a5f7d8c3b32609f555b13
supermantoman <jwg7632@gmail.com>
2023-03-08 12:38:50 +0900

    [feat] 데이터바인딩 연습, props 이해하기

---
+++ ahop/src/App.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Container, Nav} from 'react-bootstrap';
+import bg from './img/bgImage3.jpg';
+// import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import data from './data.js';
+import ListItem from './components/ListItem';
+
+function App() {
+
+  let [shoes] = useState(data);
+
+  return (
+    // Navbar
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home" className="logo">NBNST</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <br />
+
+
+    <div className='main-bg' style={{backgroundImage: 'url(' + bg + ')' }}></div>
+
+      <Row>
+        {/* <ListItem i={1} 캬={shoes[0]}/>
+        <ListItem i={2} 캬={shoes[1]}/>
+        <ListItem i={3} 캬={shoes[2]}/> */}
+        {
+          shoes.map((a,index)=>{
+            return(
+              <ListItem index={index+1} 캬={shoes[index]}  />
+            )
+          })
+        }
+      </Row>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ ahop/src/components/ListItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+import Col from 'react-bootstrap/Col';
+import data from '../data';
+
+export default function ListItem(props){
+
+  return (      
+      <Col xs>
+        <img src={'https://codingapple1.github.io/shop/shoes' + props.index + '.jpg'} width="80%" alt="" />  
+        <h4>{props.캬.title}</h4>
+        <p>{props.캬.price}</p>
+      </Col>
+    )
+}
