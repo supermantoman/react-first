fa85823dd7c5542108fe4a9eddcb7e4c5089e63f
supermantoman <jwg7632@gmail.com>
2023-03-11 00:06:34 +0900

    styled-components 라이브러리 설치 및 문법연습

--- ahop/src/pages/ProductDetail.js
+++ ahop/src/pages/ProductDetail.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+let YellowBtn = styled.button`
+  background : ${props => props.bg };
+  color : ${props => props.fontColor};
+  padding : 10px;
+  font-weight: bold;
+`
+
+let Box = styled.div`
+  background : grey;
+  padding: 20px;
+`
 
 function ProductDetail(props){
   
   let {id} = useParams();
+  let 찾은상품 = props.shoes.find(function(x){
+    return x.id == id
+  });
 
   return(
     <>
     <div className="container">
+      <Box>
+        <YellowBtn fontColor='#fff' bg='blue'>버튼</YellowBtn>
+      </Box>
     <div className="row">
       <div className="col-md-6">
         {/* {props.shoes.map((a,index)=>{  
           return <img src={"https://codingapple1.github.io/shop/shoes" + (index+1) + ".jpg"} width="100%" />;
         })} */}
-        <img src={"https://codingapple1.github.io/shop/shoes" + (parseInt(props.shoes[id].id) + 1) + ".jpg"} width="100%" />
+        <img src={"https://codingapple1.github.io/shop/shoes" + (parseInt(props.shoes[id].id + 1)) + ".jpg"} width="100%" />
       </div>
       <div className="col-md-6">
-        <h4 className="pt-5">{props.shoes[id].title}</h4>
-        <p>{props.shoes[id].content}</p>
-        <p>{props.shoes[id].price}</p>
+        <h4 className="pt-5">{찾은상품.title}</h4>
+        <p>{찾은상품.content}</p>
+        <p>{찾은상품.price}</p>
         <button className="btn btn-danger">주문하기</button> 
       </div>
     </div>
